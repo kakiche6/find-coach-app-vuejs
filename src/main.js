51752430be762b95fcab3c1c547d32759c8c5706
kakiche6 <arezki.inventeam@gmail.com>
2021-12-26 16:42:41 +0100

    optimizing using asynchronous components(lazy loading)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 import router from './router.js';
 import store from './store/index.js';
 import App from './App.vue';
-import BaseCard from './components/ui/BaseCard.vue';
-import BaseButton from './components/ui/BaseButton.vue';
-import BaseBadge from './components/ui/BaseBadge.vue';
-import BaseSpinner from './components/ui/BaseSpinner.vue';
-import BaseDialog from './components/ui/BaseDialog.vue';
 
-const app = createApp(App)
+const BaseCard = defineAsyncComponent(() =>
+  import('./components/ui/BaseCard.vue')
+);
+const BaseButton = defineAsyncComponent(() =>
+  import('./components/ui/BaseButton.vue')
+);
+const BaseBadge = defineAsyncComponent(() =>
+  import('./components/ui/BaseBadge.vue')
+);
+const BaseSpinner = defineAsyncComponent(() =>
+  import('./components/ui/BaseSpinner.vue')
+);
+const BaseDialog = defineAsyncComponent(() =>
+  import('./components/ui/BaseDialog.vue')
+);
+
+const app = createApp(App);
 
 app.use(router);
 app.use(store);
